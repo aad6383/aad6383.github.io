c01a67e5d29447729f56a02c944045b1696c167d
aad6383 <60076857+aad6383@users.noreply.github.com>
2025-11-18 05:36:28 +0000

    Deploying to gh-pages from @ aad6383/aad6383.github.io@4e2e9ff3145f2c63593587dada7c90b9da54a488 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,17 +414,7 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
+      },{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
